724aa1f4cc5ee3751c0635e8a40a380918156bf2
Sukrit Walia <sukritwalia@gmail.com>
2025-05-29 16:49:59 -0400

    Schema update.

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_05_29_012135) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_catalog.plpgsql"
-
   create_table "events", force: :cascade do |t|
     t.integer "startgg_id"
     t.string "name"
