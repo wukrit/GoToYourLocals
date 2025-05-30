3a3a420473ea50fffb658c00a6681a9658f81b2b
Sukrit Walia <sukritwalia@gmail.com>
2025-05-29 20:57:40 -0400

    Add cache migrations.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_30_002546) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_30_005534) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -41,6 +41,17 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_30_002546) do
     t.datetime "updated_at", null: false
     t.index ["event_id"], name: "index_matches_on_event_id"
     t.index ["startgg_id"], name: "index_matches_on_startgg_id", unique: true
+  end
+
+  create_table "solid_cache_entries", force: :cascade do |t|
+    t.binary "key", null: false
+    t.binary "value", null: false
+    t.datetime "created_at", null: false
+    t.bigint "key_hash", null: false
+    t.integer "byte_size", null: false
+    t.index ["byte_size"], name: "index_solid_cache_entries_on_byte_size"
+    t.index ["key_hash", "byte_size"], name: "index_solid_cache_entries_on_key_hash_and_byte_size"
+    t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash", unique: true
   end
 
   create_table "solid_queue_blocked_executions", force: :cascade do |t|
